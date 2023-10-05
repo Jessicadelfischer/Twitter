5d0f64d66f9a2958f2c75932d0efcdaeba7fbfd8
Jess <jessicadelfischer@gmail.com>
2023-10-05 16:46:21 -0300

    implements methods on and in tweet.ts :)

--- src/Classes/Tweet.ts
+++ src/Classes/Tweet.ts
@@ -1,21 +1,51 @@
 import { randomUUID } from "crypto"
-import { Like } from "./Like"
+import { tweetList, userList } from "../index"
+import { User } from "./User"
 
-export class Tweet implements Like {
-    id: string
+export class Tweet {
+    private id: string
     content: string
-    type: string
-    constructor(content: string, type: string){
+    type: 'normal' | 'reply'
+    user: string
+    replies: string[]
+    likes: string[]
+
+    constructor(content: string, user: string){
+        const foundUser = userList.find((someUser) => 
+                                    someUser.username === user)
+        if(!foundUser)
+            throw new Error("Usuário não encontrado")        
         this.id = randomUUID()
         this.content = content
-        this.type = type
+        this.type = 'normal'
+        this.user = user
+        this.replies = []
+        this.likes = []
+        tweetList.push(this)
     }
-    reply(content:any){
+    reply(content:Tweet){
+        tweetList.forEach((tweet, i) => { if (tweet === content) tweet.type = 'reply'; })
+        this.replies.push(`> @${content.user}: ${content.content}`)
     }
-    like(){
+    like(user: User){
+        if (!this.likes.includes(user.username))
+            this.likes.push(user.username)
     }
     show(){
+        const replies = this.replies.join('\n')
+        let userLikes = "[0 likes]"
+        const likes = this.likes.length
+        if (likes > 1) {
+            userLikes = `[@${this.likes[0]} and other ${likes - 1} user liked this]`
+        }
+        if (likes === 1) {
+            userLikes = `[@${this.likes[0]} liked this]`
+        }
+        console.log(`@${this.user}: ${this.content}\n${userLikes}\n ${replies}\n-----------------------------------------`)
     }
-    showRepiles(){        
+        
+    showReplies() {
+        const join = this.replies.join('\n')
+        console.log(join)
     }
 }
