e2f32e0fcbb6477e3d603b3b8799e8cef602e0ae
Jessica de Lurdes Fischer <104735684+Jessicadelfischer@users.noreply.github.com>
2023-10-08 20:29:39 -0300

    Transformer (#6)
    * delete like.ts
    
    * implements methods on and in tweet.ts :)
    
    * adjust to size show
    
    * delete service
    
    * update user.ts
    
    * update index.ts
    
    * index not used
    
    * remove semicolon
    
    * Update Tweet.ts
    
    * Index novo
    
    * hotfixs
    
    * Finish
    
    OK

--- src/Classes/User.ts
+++ src/Classes/User.ts
@@ -1,26 +1,47 @@
 import { randomUUID } from "crypto"
+import { tweetList, userList } from "../index"
+import { Tweet } from "./Tweet"
+
 export class User {
     private id: string
     name: string
     username: string
     email: string
     password: string
+    following: User[]
 
     constructor (name: string, username: string, email: string, password: string){
+        const foundUser = userList
+                        .find((someUser) => someUser.username === username)
+        if(foundUser)
+            throw new Error("username já existente")        
         this.id = randomUUID()
         this.name = name
         this.username = username
         this.email = email
         this.password = password
+        this.following = []
+        userList.push(this)
     }
 
-    sendTweet(tweet: any){
+    sendTweet(tweet: string){
+    new Tweet(tweet, this.username)
+    }    
+    
+    follow(user: User){
+        user !== this ?
+            this.following.push(user) :       
+            console.error("Não é possível seguir a si mesmo")
     }
-    follow(user: any){
-    }
+
     showFeed(){
+        const usernames = this.following.map((user) => user.username)
+        const normal = tweetList.filter((tweet) => tweet.type !== "reply")
+        const feed = normal.filter((tweet) => tweet.user === this.username || usernames.includes(tweet.user))
+        feed.map(tweet => tweet.show()).join('\n')
     }
     showTweets(){
+        const feed = tweetList.filter((tweet) => tweet.user === this.username)
+        feed.map(tweet => tweet.show()).join('\n')        
     }
-
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,43 @@
-import { User } from "./Classes/User"
 import { Tweet } from "./Classes/Tweet"
-import { createUser } from "./Service/UserService"
+import { User } from "./Classes/User"
 
 export const userList: User[] = []
 export const tweetList: Tweet[] = []
-export const followList: any[] = []    
 
-createUser("Amanda", "Mandita", "lopesamanda@gmail.com", "123456")
+const usuario = new User("João", "joao", "joca@gmail.com", "123456")
+const usuario2 = new User("Maria", "maria", "maria@gmail.com", "123456")
+const usuario3 = new User("Pedro", "pedro", "pedro@gmail.com", "123456")
+const usuario4 = new User("Julia", "juh", "juliacastro@hotmail.com", "balabola")
+const tweett = new Tweet("Oba cheguei", "juh")
+const tweet = new Tweet("Olá, mundo!", "joao")
+const tweet2 = new Tweet("Tô na área", "maria")
+const tweet3 = new Tweet("Olá, univero!!!!", "pedro")
+const tweet4 = new Tweet("HI GUYS!!!", "pedro")
+
+usuario.follow(usuario2)
+usuario.follow(usuario3)
+usuario2.follow(usuario)
+usuario3.follow(usuario)
+usuario4.follow(usuario)
 
 
+tweett.like(usuario)
+tweett.like(usuario2)
+tweett.like(usuario3)
+tweett.reply("Eu tbm cheguei", "maria")
+tweet3.reply("Olá, Galaxia", "joao")
 
 
+tweett.show()
+tweet2.like(usuario)
+tweet2.show()
+
+usuario.sendTweet("Vamos lá!")
+
+console.log("____________________Show Feed_________________________")
+usuario.showFeed()
+console.log("____________________Show Tweets ______________________")
+usuario.showTweets()
+console.log("____________________Show Replies de tweet_____________")
+tweet3.showReplies()
+tweett.showReplies()
