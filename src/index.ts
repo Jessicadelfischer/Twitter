57372cdfce894fd540a7e1cba8c719020dc2a173
Jessica de Lurdes Fischer <jessicadelfischer@gmail.com>
2023-10-08 19:35:51 -0300

    Index novo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { send } from "process"
 import { Tweet } from "./Classes/Tweet"
 import { User } from "./Classes/User"
+import { Console } from "console"
 
 export const userList: User[] = []
 export const tweetList: Tweet[] = []
@@ -7,39 +9,35 @@ export const tweetList: Tweet[] = []
 const usuario = new User("João", "joao", "joca@gmail.com", "123456")
 const usuario2 = new User("Maria", "maria", "maria@gmail.com", "123456")
 const usuario3 = new User("Pedro", "pedro", "pedro@gmail.com", "123456")
+const usuario4 = new User("Julia", "juh", "juliacastro@hotmail.com", "balabola")
+const tweett = new Tweet("Oba cheguei", "juh")
 const tweet = new Tweet("Olá, mundo!", "joao")
-const tweet2 = new Tweet("Olá, mundo!", "maria")
-const tweet3 = new Tweet("Olá, mundo!", "pedro")
+const tweet2 = new Tweet("Tô na área", "maria")
+const tweet3 = new Tweet("Olá, univero!!!!", "pedro")
+const tweet4 = new Tweet("HI GUYS!!!", "pedro")
 
-console.log("\nprimeiro tweet show\n***********************************************************************************************")
-tweet.show()
-tweet.reply(new Tweet("reply aqui", "joao"))
-console.log("\ntweet show reply 1\n***********************************************************************************************")
-tweet.show()
-tweet.reply(new Tweet("reply aqui 2", "maria"))
-tweet.reply(new Tweet("reply aqui 3", "pedro"))
-console.log("\ntweet show reply 2\n***********************************************************************************************")
-tweet.show()
-tweet.like(usuario)
-tweet.like(usuario)
-console.log("\ntweets show like 1\n***********************************************************************************************")
-tweet.show()
-tweet.like(usuario2)
-console.log("\ntweets show like 2\n***********************************************************************************************")
-tweet.show()
-tweet.like(usuario3)
-console.log("\ntweets show like 3\n***********************************************************************************************")
-tweet.show()
-
-console.log("\nfim show\n***********************************************************************************************")
-console.log("\napenas replies\n***********************************************************************************************")
-tweet.showReplies()
-console.log("\nfim replies\n***********************************************************************************************")
-
-console.log("\nfeed\n***********************************************************************************************")
 usuario.follow(usuario2)
 usuario.follow(usuario3)
-usuario.showFeed()
-console.log("\nmeus tweets\n***********************************************************************************************")
-usuario.showTweets()
+usuario2.follow(usuario)
+usuario3.follow(usuario)
+usuario4.follow(usuario)
 
+
+tweett.reply(tweet)
+tweett.reply(tweet2)
+tweet3.reply(tweett)
+tweett.like(usuario)
+tweett.like(usuario2)
+tweett.like(usuario3)
+
+
+tweett.show()
+tweet2.like(usuario)
+tweet2.show()
+
+tweet3.showReplies()
+tweett.showReplies()
+console.log("________Tweets do usuário_____________")
+usuario.showFeed()
+console.log("________Tweets _____________")
+usuario.showTweets()
